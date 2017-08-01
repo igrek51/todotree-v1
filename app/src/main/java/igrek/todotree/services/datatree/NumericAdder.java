2238fb62293449d57821a074ce3deeda08d1867c
iszulc <iszulc@relayonit.pl>
2017-08-01 14:03:27 +0200

    Add NumericAdder

---
+++ app/src/main/java/igrek/todotree/services/datatree/NumericAdder.java
@@ -0,0 +1,53 @@
+package igrek.todotree.services.datatree;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class NumericAdder {
+	
+	BigDecimal sumSelected(List<Integer> selectedPositions, TreeItem currentItem) throws NumberFormatException {
+		BigDecimal sum = new BigDecimal(0);
+		for (Integer selectedPos : selectedPositions) {
+			TreeItem selectedItem = currentItem.getChild(selectedPos);
+			BigDecimal itemValue = getItemNumericValue(selectedItem.getContent());
+			if (itemValue != null) {
+				sum = sum.add(itemValue);
+			}
+		}
+		return sum;
+	}
+	
+	private BigDecimal getItemNumericValue(String content) throws NumberFormatException {
+		
+		String valueStr = null;
+		
+		content = content.replace(',', '.');
+		
+		Pattern pattern = Pattern.compile("^(-? ?\\d+(\\.\\d+)?)(.*?)$");
+		Matcher matcher = pattern.matcher(content);
+		if (matcher.matches()) {
+			valueStr = matcher.group(1);
+			valueStr = valueStr.replaceAll(" ", "");
+		} else {
+			pattern = Pattern.compile("^(.*?)(-? ?\\d+(\\.\\d+)?)$");
+			matcher = pattern.matcher(content);
+			if (matcher.matches()) {
+				valueStr = matcher.group(2);
+				valueStr = valueStr.replaceAll(" ", "");
+			}
+		}
+		
+		if (valueStr != null) {
+			try {
+				return new BigDecimal(valueStr);
+			} catch (NumberFormatException e) {
+				throw new NumberFormatException("Invalid number format:\n" + valueStr);
+			}
+		} else {
+			throw new NumberFormatException("No number value:\n" + content);
+		}
+	}
+}
--- app/src/main/java/igrek/todotree/services/datatree/TreeManager.java
+++ app/src/main/java/igrek/todotree/services/datatree/TreeManager.java
@@ -6,8 +6,6 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import igrek.todotree.exceptions.NoSuperItemException;
 import igrek.todotree.logger.Logs;
@@ -35,6 +33,8 @@ public class TreeManager {
 	private List<TreeItem> clipboard = null;
 	
 	private HashMap<TreeItem, Integer> storedScrollPositions;
+	
+	private NumericAdder numericAdder = new NumericAdder();
 	
 	public TreeManager(FilesystemService filesystem, Preferences preferences, TreeSerializer treeSerializer) {
 		this.filesystem = filesystem;
@@ -345,48 +345,7 @@ public class TreeManager {
 		return storedScrollPositions.get(item);
 	}
 	
-	// sumowanie wartości pozycji
-	
-	public BigDecimal sumSelected() throws NumberFormatException {
-		BigDecimal sum = new BigDecimal(0);
-		for (Integer selectedPos : selectedPositions) {
-			TreeItem selectedItem = getCurrentItem().getChild(selectedPos);
-			BigDecimal itemValue = getItemNumericValue(selectedItem.getContent());
-			if (itemValue != null) {
-				sum = sum.add(itemValue);
-			}
-		}
-		return sum;
-	}
-	
-	private BigDecimal getItemNumericValue(String content) throws NumberFormatException {
-		
-		String valueStr = null;
-		
-		content = content.replace(',', '.');
-		
-		Pattern pattern = Pattern.compile("^(-? ?\\d+(\\.\\d+)?)(.*?)$");
-		Matcher matcher = pattern.matcher(content);
-		if (matcher.matches()) {
-			valueStr = matcher.group(1);
-			valueStr = valueStr.replaceAll(" ", "");
-		} else {
-			pattern = Pattern.compile("^(.*?)(-? ?\\d+(\\.\\d+)?)$");
-			matcher = pattern.matcher(content);
-			if (matcher.matches()) {
-				valueStr = matcher.group(2);
-				valueStr = valueStr.replaceAll(" ", "");
-			}
-		}
-		
-		if (valueStr != null) {
-			try {
-				return new BigDecimal(valueStr);
-			} catch (NumberFormatException e) {
-				throw new NumberFormatException("Invalid number format:\n" + valueStr);
-			}
-		} else {
-			throw new NumberFormatException("No number value:\n" + content);
-		}
+	public BigDecimal sumSelected() {
+		return numericAdder.sumSelected(selectedPositions, getCurrentItem());
 	}
 }
